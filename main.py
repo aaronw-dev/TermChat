85860fda7ab8e5c0a93c41a2d467610a31edd21a
AW_Dev <riptidegamesceo@gmail.com>
2023-09-10 15:53:01 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import json
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, send, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+opensockets = []
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+@socketio.on('connect')
+def test_connect():
+    print("Somebody connected.")
+    emit('onconnect', {'data': 'Connected'})
+
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+
+@socketio.on('message')
+def handle_message(data):
+    message = data["data"]
+    message = message.strip()
+    print('received message: ' + message)
+    responsejson = {
+        "author": "server",
+        "content": "This is what you said: \"" + message + "\" at " + datetime.now().strftime("%H:%M"),
+        "timestamp": datetime.now().timestamp()
+    }
+    emit("serverresponse", responsejson)
+
+
+@app.errorhandler(500)
+def fivehundrederror(error):
+    return render_template("error.html", errorcode=error)
+
+
+@app.errorhandler(404)
+def invalid_route(error):
+    return render_template("error.html")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    print("Running now.")
+    socketio.run(app, host='0.0.0.0', port=5000)
