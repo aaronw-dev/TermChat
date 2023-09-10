fa64fe8bd5e48317870803c8a8e2cdee5097d34e
AW_Dev <riptidegamesceo@gmail.com>
2023-09-10 16:30:37 -0700

    Added colored logging

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from datetime import datetime
 import json
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send, emit
-
+from colors import *
+import os
+os.system("")
 app = Flask(__name__)
 socketio = SocketIO(app)
 
@@ -16,26 +18,26 @@ def homepage():
 
 @socketio.on('connect')
 def test_connect():
-    print("Somebody connected.")
+    print(CGREEN2 + "Client connected." + CEND)
     emit('onconnect', {'data': 'Connected'})
 
 
 @socketio.on('disconnect')
 def test_disconnect():
-    print('Client disconnected')
+    print(CRED + 'Client disconnected' + CEND)
 
 
 @socketio.on('message')
 def handle_message(data):
     message = data["data"]
     message = message.strip()
-    print('received message: ' + message)
+    print('received message: ' + CBLUE + message + CEND)
     responsejson = {
         "author": "server",
         "content": "This is what you said: \"" + message + "\" at " + datetime.now().strftime("%H:%M"),
         "timestamp": datetime.now().timestamp()
     }
-    emit("serverresponse", responsejson)
+    emit("serverresponse", responsejson, broadcast=True)
 
 
 @app.errorhandler(500)
@@ -54,5 +56,5 @@ def index():
 
 
 if __name__ == '__main__':
-    print("Running now.")
+    print(CYELLOW + "Running now." + CEND)
     socketio.run(app, host='0.0.0.0', port=5000)
